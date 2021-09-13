3f9e1c0c2eeb99c55cb32383eed79e3aaa29863f
Yogo772 <90017010+Yogo772@users.noreply.github.com>
2021-09-13 11:12:48 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,15 +18,15 @@ const app = Vue.createApp({
       */
       correctAnswer: {
         stage1: {
-          q1: 'あああ',
+          q1：'ロゴ’、
         },
         stage2: {
-          q1: 'いいい',
+          q1: '2063',
           // q2: 'えええ',
           // q3: 'おおお'
         },
         stage3: {
-          q1: 'ううう',
+          q1:'PROTECT'、
           // q2: 'かかか',
           // q3: 'ききき',
         }
